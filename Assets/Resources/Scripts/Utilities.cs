66b8c265a18d4e8bfe13575d70ae216d1c77364d
thedonaldloughry <dloughry32@gmail.com>
2016-01-29 21:44:30 -0500

    Trying a second commit. - Don

---
+++ Assets/Resources/Scripts/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game_Utils
+{
+    public interface ISceneObject
+    {
+        // Called by the GameBrain on scene switch.
+        void Initialize();
+        // Called by the GameBrain's Update function.
+        void ObjectUpdate();
+    }
+    public static class Utilities
+    {
+
+    }
+}
+
