fafd5a7381e42a6fd7dfa309e95a34dd1b2ab817
new-00-0ne <caleb.matthew.brown@gmail.com>
2016-01-31 04:48:18 -0500

    Re-Organized level Hierarchy

--- Assets/Dusty Char/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Dusty Char/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,11 +6,12 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+        public string levelName;
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.tag == "Player")
+            if (other.tag == "Scene_Object")
             {
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).path);
+                SceneManager.LoadScene(levelName);
             }
         }
     }
