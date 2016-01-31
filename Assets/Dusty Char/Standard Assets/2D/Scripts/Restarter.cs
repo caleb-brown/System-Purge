e82cc7409099c53e267d411e77deaa7e7facc68c
CoryAlanDavis <cory.davisalan@yahoo.com>
2016-01-31 00:16:58 -0500

    commitment the battery health gui thing for Noland. also fixed kill boxes so that they can be given a name.

--- Assets/Dusty Char/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Dusty Char/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,11 +6,12 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+        public string mapName; // does not need extension. will error if provided
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).path);
+                SceneManager.LoadScene(mapName);
             }
         }
     }
